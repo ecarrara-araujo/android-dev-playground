9e159d379bc3b97ddccd28ff3228596c083b6b7c
Eduardo Carrara <ecarrara.araujo@gmail.com>
2020-07-05 16:06:05 +0100

    [coroutines-codelab] Extract a launchDataLoad utility

--- coroutines-codelabs/coroutines-codelab/start/src/main/java/com/example/android/kotlincoroutines/main/MainViewModel.kt
+++ coroutines-codelabs/coroutines-codelab/start/src/main/java/com/example/android/kotlincoroutines/main/MainViewModel.kt
@@ -22,6 +22,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.android.kotlincoroutines.util.BACKGROUND
 import com.example.android.kotlincoroutines.util.singleArgViewModelFactory
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
@@ -122,11 +123,16 @@ class MainViewModel(private val repository: TitleRepository) : ViewModel() {
      * Refresh the title, showing a loading spinner while it refreshes and errors via snackbar.
      */
     fun refreshTitle() {
-        // TODO: Convert refreshTitle to use coroutines
-        viewModelScope.launch {
+        launchDataLoad {
+            repository.refreshTitle()
+        }
+    }
+
+    private fun launchDataLoad(block: suspend () -> Unit): Job {
+        return viewModelScope.launch {
             try {
                 _spinner.value = true
-                repository.refreshTitle()
+                block()
             } catch (error: TitleRefreshError) {
                 _snackBar.postValue(error.message)
             } finally {
