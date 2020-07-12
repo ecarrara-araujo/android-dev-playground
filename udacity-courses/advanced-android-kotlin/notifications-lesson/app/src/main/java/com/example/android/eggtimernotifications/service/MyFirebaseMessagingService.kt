58fc06282e07c7c0d37c15b0ec013d6cbcdf35cf
Eduardo Carrara <ecarrara.araujo@gmail.com>
2020-07-12 21:59:04 +0100

    [udacity-aak] Data messages and notifications

--- udacity-courses/advanced-android-kotlin/notifications-lesson/app/src/main/java/com/example/android/eggtimernotifications/service/MyFirebaseMessagingService.kt
+++ udacity-courses/advanced-android-kotlin/notifications-lesson/app/src/main/java/com/example/android/eggtimernotifications/service/MyFirebaseMessagingService.kt
@@ -1,10 +1,15 @@
 package com.example.android.eggtimernotifications.service
 
-import android.content.ContentValues.TAG
+import android.app.NotificationManager
 import android.util.Log
+import androidx.core.content.ContextCompat
+import com.example.android.eggtimernotifications.util.sendNotification
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
+    private val TAG = MyFirebaseMessagingService::class.java.simpleName
+
     // Step 3.2 log registration token
     // [START on_new_token]
     /**
@@ -13,7 +18,7 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
      * is initially generated so this is where you would retrieve the token.
      */
     override fun onNewToken(token: String?) {
-        Log.d(MyFirebaseMessagingService::class.java.simpleName, "Refreshed token: $token")
+        Log.d(TAG, "Refreshed token: $token")
 
         // If you want to send messages to this application instance or
         // manage this apps subscriptions on the server side, send the
@@ -21,4 +26,29 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         // sendRegistrationToServer(token)
     }
     // [END on_new_token]
+
+    // [START receive_message]
+    override fun onMessageReceived(remoteMessage: RemoteMessage?) {
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+        Log.d(TAG, "From: ${remoteMessage?.from}")
+
+        // Step 3.5 check messages for data
+        // Check if message contains a data payload.
+        remoteMessage?.data?.let {
+            Log.d(TAG, "Message data payload: " + remoteMessage.data)
+        }
+
+        // Step 3.6 check messages for notification and call sendNotification
+        // Check if message contains a notification payload.
+        remoteMessage?.notification?.let {
+            Log.d(TAG, "Message Notification Body: ${it.body}")
+            sendNotification(it.body.orEmpty())
+        }
+    }
+    // [END receive_message]
+
+    private fun sendNotification(message: String) {
+        ContextCompat.getSystemService(applicationContext, NotificationManager::class.java)
+            ?.sendNotification(message, applicationContext)
+    }
 }
