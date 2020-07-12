aa28d4d31884311b44e0e35c4737b2e37fac88fd
Eduardo Carrara <ecarrara.araujo@gmail.com>
2020-07-12 20:41:22 +0100

    [udacity-aak] PendingIntent to open the app from the notification

--- udacity-courses/advanced-android-kotlin/notifications-lesson/app/src/main/java/com/example/android/eggtimernotifications/util/NotificationUtils.kt
+++ udacity-courses/advanced-android-kotlin/notifications-lesson/app/src/main/java/com/example/android/eggtimernotifications/util/NotificationUtils.kt
@@ -40,9 +40,16 @@ private val FLAGS = 0
 fun NotificationManager.sendNotification(messageBody: String, applicationContext: Context) {
     // Create the content intent for the notification, which launches
     // this activity
-    // TODO: Step 1.11 create intent
+    // Step 1.11 create intent
+    val contentIntent = Intent(applicationContext, MainActivity::class.java)
 
-    // TODO: Step 1.12 create PendingIntent
+    // Step 1.12 create PendingIntent
+    val contentPendingIntent = PendingIntent.getActivity(
+        applicationContext,
+        NOTIFICATION_ID,
+        contentIntent,
+        PendingIntent.FLAG_UPDATE_CURRENT
+    )
 
     // TODO: Step 2.0 add style
 
@@ -61,8 +68,9 @@ fun NotificationManager.sendNotification(messageBody: String, applicationContext
         .setSmallIcon(R.drawable.cooked_egg)
         .setContentTitle(applicationContext.getString(R.string.notification_title))
         .setContentText(messageBody)
-
-    // TODO: Step 1.13 set content intent
+    // Step 1.13 set content intent
+        .setContentIntent(contentPendingIntent)
+        .setAutoCancel(true)
 
         // TODO: Step 2.1 add style to builder
 
