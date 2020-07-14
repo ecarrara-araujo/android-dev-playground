43446acec5b07f0e09f282882a2335f12e76c6f4
Eduardo Carrara <ecarrara.araujo@gmail.com>
2020-07-14 21:19:16 +0100

    [udacity-aak] Enables DialView to handle clicks

--- udacity-courses/advanced-android-kotlin/custom-views-lesson/app/src/main/java/br/com/ecarrara/customfancontroller/DialView.kt
+++ udacity-courses/advanced-android-kotlin/custom-views-lesson/app/src/main/java/br/com/ecarrara/customfancontroller/DialView.kt
@@ -13,7 +13,14 @@ private enum class FanSpeed(val label: Int) {
     OFF(R.string.fan_off),
     LOW(R.string.fan_low),
     MEDIUM(R.string.fan_medium),
-    HIGH(R.string.fan_high),
+    HIGH(R.string.fan_high);
+
+    fun next() = when (this) {
+        OFF -> LOW
+        LOW -> MEDIUM
+        MEDIUM -> HIGH
+        HIGH -> OFF
+    }
 }
 
 private const val RADIUS_OFFSET_LABEL = 30
@@ -34,6 +41,10 @@ class DialView @JvmOverloads constructor(
         textAlign = Paint.Align.CENTER
         textSize = 55.0f
         typeface = Typeface.create("", Typeface.BOLD)
+    }
+
+    init {
+        isClickable = true
     }
 
     override fun onSizeChanged(width: Int, height: Int, oldWidth: Int, oldHeight: Int) {
@@ -58,6 +69,16 @@ class DialView @JvmOverloads constructor(
             val label = resources.getString(i.label)
             canvas?.drawText(label, pointPosition.x, pointPosition.y, paint)
         }
+    }
+
+    override fun performClick(): Boolean {
+        if (super.performClick()) return true
+
+        fanSpeed = fanSpeed.next()
+        contentDescription = resources.getString(fanSpeed.label)
+
+        invalidate()
+        return true
     }
 
     private fun PointF.computeXYForSpeed(position: FanSpeed, radius: Float) {
