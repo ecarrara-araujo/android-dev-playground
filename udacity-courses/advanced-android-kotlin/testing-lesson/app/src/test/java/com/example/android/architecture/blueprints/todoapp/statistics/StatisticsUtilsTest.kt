7fcafdfadc98321a8be2546cb64338ce3852b038
Eduardo Carrara <ecarrara.araujo@gmail.com>
2020-08-13 21:48:30 +0100

    [udacity-akk] Write few unit tests for getActiveAndCompletedStat

---
+++ udacity-courses/advanced-android-kotlin/testing-lesson/app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
@@ -0,0 +1,44 @@
+package com.example.android.architecture.blueprints.todoapp.statistics
+
+import com.example.android.architecture.blueprints.todoapp.data.Task
+import org.junit.Assert.*
+import org.junit.Test
+
+class StatisticsUtilsTest {
+
+    @Test
+    fun getActiveAndCompletedStats_calledWithNoCompletedTasks_returns0CompletedAnd100Active() {
+        val tasks = listOf(Task("title", "desc", isCompleted = false))
+
+        val result = getActiveAndCompletedStats(tasks)
+
+        assertEquals(0f, result.completedTasksPercent)
+        assertEquals(100f, result.activeTasksPercent)
+    }
+
+    @Test
+    fun getActiveAndCompletedTasks_calledWithOnlyCompletedTasks_returns100CompletedAnd0Active() {
+        val tasks = listOf(Task("title", "desc", isCompleted = true))
+
+        val result = getActiveAndCompletedStats(tasks)
+
+        assertEquals(100f, result.completedTasksPercent)
+        assertEquals(0f, result.activeTasksPercent)
+    }
+
+    @Test
+    fun getActiveAndCompletedTasks_calledWith2CompletedAnd3ActiveTasks_returns40CompletedAnd60Active() {
+        val tasks = listOf(
+                Task("title", "desc", isCompleted = true),
+                Task("title", "desc", isCompleted = true),
+                Task("title", "desc", isCompleted = false),
+                Task("title", "desc", isCompleted = false),
+                Task("title", "desc", isCompleted = false)
+        )
+
+        val result = getActiveAndCompletedStats(tasks)
+
+        assertEquals(40f, result.completedTasksPercent)
+        assertEquals(60f, result.activeTasksPercent)
+    }
+}
