dfbfe68102e2dd339adaef21787b5488739bb43c
Eduardo Carrara <ecarrara.araujo@gmail.com>
2020-09-09 21:34:16 +0100

    [compose-basics] Design a basic layout with text, image and spacer in a column

--- jetpack-compose/ComposeBasics/app/src/main/java/br/com/ecarrara/composebasics/MainActivity.kt
+++ jetpack-compose/ComposeBasics/app/src/main/java/br/com/ecarrara/composebasics/MainActivity.kt
@@ -2,37 +2,49 @@ package br.com.ecarrara.composebasics
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.Text
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Preview
-import br.com.ecarrara.composebasics.ui.ComposeBasicsTheme
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeBasicsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            NewsStory()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+private fun NewsStory() {
+    val image = imageResource(id = R.drawable.header)
+
+    Column(Modifier.padding(16.dp)) {
+        Image(
+                image,
+                Modifier
+                        .preferredHeight(180.dp)
+                        .fillMaxWidth(),
+                contentScale = ContentScale.Crop
+        )
+
+        Spacer(Modifier.preferredHeight(16.dp))
+
+        Text(text = "A day in Shark Fin Cove")
+        Text(text = "Davenport, California")
+        Text(text = "December 2018")
+    }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun DefaultPreview() {
-    ComposeBasicsTheme {
-        Greeting("Android")
-    }
+private fun DefaultPreview() {
+    NewsStory()
 }
