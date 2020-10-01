5549e339d4c5b9e676c7cf369407b5670ffa734e
Eduardo Carrara <ecarrara.araujo@gmail.com>
2020-10-01 10:05:30 +0100

    [compose-codelab-basic] Basic composable functions

--- jetpack-compose/compose-codelab-basics/app/src/main/java/br/com/ecarrara/composecodelabbasics/MainActivity.kt
+++ jetpack-compose/compose-codelab-basics/app/src/main/java/br/com/ecarrara/composecodelabbasics/MainActivity.kt
@@ -3,10 +3,16 @@ package br.com.ecarrara.composecodelabbasics
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Preview
 import br.com.ecarrara.composecodelabbasics.ui.ComposeCodelabBasicsTheme
 
@@ -14,25 +20,43 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeCodelabBasicsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+            MyApp {
+                MyScreenContent()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MyApp(content: @Composable () -> Unit) {
+    ComposeCodelabBasicsTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(color = MaterialTheme.colors.background) {
+            content()
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+}
+
+@Composable
+fun MyScreenContent(names: List<String> = listOf("Android", "there")) {
+    Column {
+        for (name in names) {
+            Greeting(name = name)
+            Divider(color = Color.Black)
+        }
+    }
+}
+
+
+@Preview(showBackground = true, name = "Text Preview")
 @Composable
 fun DefaultPreview() {
-    ComposeCodelabBasicsTheme {
-        Greeting("Android")
+    MyApp {
+        MyScreenContent()
     }
 }
