0967a25cc86ed9f2a6719e7c49ae3c73917d542a
Eduardo Carrara <ecarrara.araujo@gmail.com>
2020-10-15 09:44:11 +0100

    [compose-codelab-layout] Material Theme Components and Layout

--- jetpack-compose/compose-codelab-layout/app/src/main/java/br/com/ecarrara/compose_codelab_layout/MainActivity.kt
+++ jetpack-compose/compose-codelab-layout/app/src/main/java/br/com/ecarrara/compose_codelab_layout/MainActivity.kt
@@ -2,22 +2,39 @@ package br.com.ecarrara.compose_codelab_layout
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Icon
 import androidx.compose.foundation.Text
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.preferredSize
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBalanceWallet
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Preview
-import br.com.ecarrara.compose_codelab_layout.ui.ComposecodelablayoutTheme
+import br.com.ecarrara.compose_codelab_layout.ui.ComposeCodelabLayoutTheme
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposecodelablayoutTheme {
+            ComposeCodelabLayoutTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    LayoutsCodelab()
                 }
             }
         }
@@ -25,14 +42,85 @@ class MainActivity : AppCompatActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun LayoutsCodelab() {
+    Scaffold(
+            topBar = {
+                TopAppBar(
+                        title = {
+                            Text(text = "LayoutsCodelab")
+                        },
+                        actions = {
+                            IconButton(onClick = { /* Do nothing for now */ }) {
+                                Icon(Icons.Filled.AccountBalanceWallet)
+                            }
+                        }
+                )
+            },
+            bottomBar = {
+                BottomAppBar {
+                    IconButton(onClick = { /* Do nothing for now */ }) {
+                        Icon(Icons.Filled.Star)
+                    }
+                }
+            },
+            drawerContent = {
+                Text(text = "Wololo menu")
+            }
+    ) { innerPadding ->
+        BodyContent(Modifier.padding(innerPadding).padding(8.dp))
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    ComposecodelablayoutTheme {
-        Greeting("Android")
+fun BodyContent(modifier: Modifier = Modifier) {
+    Column(modifier) {
+        Text(text = "Hi there!")
+        Text(text = "Thanks for going through the Layouts Codelab")
+    }
+}
+
+@Preview
+@Composable
+fun LayoutsCodelabPreview() {
+    ComposeCodelabLayoutTheme {
+        LayoutsCodelab()
+    }
+}
+
+
+@Composable
+fun PhotographerCard(modifier: Modifier = Modifier) {
+    Row(modifier
+            .padding(8.dp)
+            .clip(RoundedCornerShape(4.dp))
+            .background(MaterialTheme.colors.surface)
+            .clickable(onClick = { /* Does nothing for now */ })
+            .padding(16.dp)
+    ) {
+        Surface(
+                modifier = Modifier.preferredSize(50.dp),
+                shape = CircleShape,
+                color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
+        ) {
+
+        }
+        Column(
+                modifier = Modifier
+                        .padding(start = 8.dp)
+                        .align(Alignment.CenterVertically)
+        ) {
+            Text(text = "Alfred Sisley", fontWeight = FontWeight.Bold)
+            ProvideEmphasis(AmbientEmphasisLevels.current.medium) {
+                Text(text = "3 minutes ago", style = MaterialTheme.typography.body2)
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PhotographerCardPreview() {
+    ComposeCodelabLayoutTheme {
+        PhotographerCard()
     }
 }
--- jetpack-compose/compose-codelab-layout/app/src/main/java/br/com/ecarrara/compose_codelab_layout/ui/Theme.kt
+++ jetpack-compose/compose-codelab-layout/app/src/main/java/br/com/ecarrara/compose_codelab_layout/ui/Theme.kt
@@ -28,7 +28,7 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun ComposecodelablayoutTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
+fun ComposeCodelabLayoutTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
