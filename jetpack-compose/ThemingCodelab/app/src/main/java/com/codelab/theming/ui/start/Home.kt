f599e02a2208193e5c7209a6ead542282d134a71
Eduardo Carrara <ecarrara.araujo@gmail.com>
2020-10-26 10:35:29 +0000

    [compose-codelab-theming] Playing with typography

--- jetpack-compose/ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/Home.kt
+++ jetpack-compose/ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/Home.kt
@@ -28,6 +28,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.annotatedString
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Preview
 import com.codelab.theming.R
@@ -86,6 +87,7 @@ fun Header(
     ) {
         Text(
             text = text,
+            style = MaterialTheme.typography.subtitle2,
             modifier = modifier
                 .fillMaxWidth()
                 .padding(horizontal = 16.dp, vertical = 8.dp)
@@ -137,6 +139,9 @@ private fun PostMetadata(
 ) {
     val divider = "  •  "
     val tagDivider = "  "
+    val tagStyle = MaterialTheme.typography.overline.toSpanStyle().copy(
+        background = MaterialTheme.colors.primary.copy(alpha = 0.1f)
+    )
     val text = annotatedString {
         append(post.metadata.date)
         append(divider)
@@ -146,7 +151,9 @@ private fun PostMetadata(
             if (index != 0) {
                 append(tagDivider)
             }
-            append(" ${tag.toUpperCase()} ")
+            withStyle(tagStyle) {
+                append(" ${tag.toUpperCase()} ")
+            }
         }
     }
     Text(
